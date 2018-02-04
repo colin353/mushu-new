c6a2164c66654da8a3d511fe61183bced97133ff
Colin Merkel <colin.merkel@podkarma.com>
2018-02-03 18:34:53 -0800

    Add trading stage to Elm.

--- js/Api.elm
+++ js/Api.elm
@@ -45,6 +45,9 @@ actionHelp a =
                                 "auction" ->
                                     D.succeed AuctionStageType
 
+                                "trade" ->
+                                    D.succeed TradeStageType
+
                                 _ ->
                                     D.fail "Unrecognized stage name"
                         )
