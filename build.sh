0b4acd6badd415bf8536946d5f6304dcc9966bc3
Colin Merkel <colin.merkel@podkarma.com>
2018-02-04 11:30:50 -0800

    Compile the elm output to an HTML file.

--- build.sh
+++ build.sh
@@ -6,4 +6,8 @@ mkdir -p web/
 mkdir -p bin/
 
 # Build the Go binary.
-cd server; go build -o ../bin/server
+cd server; go build -o ../bin/server; cd ..
+
+# Build the elm outputs.
+cd js; elm-make --output=../web/index.html; cd ..
+
